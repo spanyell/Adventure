14aa1e42d2a97ee4f2fc6b92766c8ad0dc3061ad
Dan Beers <spanyell@icloud.com>
2021-02-11 13:41:21 -0800

    Initial Commit

---
+++ Adventure/AdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventureApp.swift
+//  Adventure
+//
+//  Created by Dan Beers on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
